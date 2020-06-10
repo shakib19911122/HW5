ed769e2612ae61afda4cd45c3574e7f78d953b74
shakib <shakib1122@gmail.com>
2020-06-10 21:50:59 +1000

    first update

---
+++ Develop/script.js
@@ -0,0 +1,13 @@
+// create onclck even
+    // when saveBtn is clicked, store the userInput in the local storage
+
+// $("#sB1").on("click", function() {
+//     alert("btn save");
+// })
+
+document.querySelector("#sB1").addEventListener("click", function() {
+    console.log("saved");
+  });
+
+
+
