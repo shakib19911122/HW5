6cf0772e9c0c6a093fc51b5defbee62f9b9b1ae0
shakib <shakib1122@gmail.com>
2020-06-11 07:21:30 +1000

    1

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 // create onclck even
+
+
+
     // when saveBtn is clicked, store the userInput in the local storage
 
-// $("#sB1").on("click", function() {
-//     alert("btn save");
-// })
+$("#sB9").on("click", function() {
+    console.log("save");
+});
+$("#sB10").on("click", function() {
+    console.log("save");
+});
+$("#sB11").on("click", function() {
+    console.log("save");
+});
+$("#sB12").on("click", function() {
+    console.log("save");
+});
+$("#sB1").on("click", function() {
+    console.log("save");
+});
+$("#sB2").on("click", function() {
+    console.log("save");
+});
+$("#sB3").on("click", function() {
+    console.log("save");
+});
+$("#sB4").on("click", function() {
+    console.log("save");
+});
+$("#sB5").on("click", function() {
+    console.log("save");
+});
 
-document.querySelector("#sB1").addEventListener("click", function() {
-    console.log("saved");
-  });
+
 
 
 
