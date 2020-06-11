0b097cc8dd278276a986d08812993d217dd0cb52
shakib <shakib1122@gmail.com>
2020-06-12 06:44:27 +1000

    morning update

--- script.js
+++ script.js
@@ -4,33 +4,41 @@
 
     // when saveBtn is clicked, store the userInput in the local storage
 
+
+
+
 $("#sB9").on("click", function() {
-    console.log("save");
+  
+    
 });
 $("#sB10").on("click", function() {
-    console.log("save");
+    
 });
-$("#sB11").on("click", function() {
-    console.log("save");
-});
-$("#sB12").on("click", function() {
-    console.log("save");
-});
-$("#sB1").on("click", function() {
-    console.log("save");
-});
-$("#sB2").on("click", function() {
-    console.log("save");
-});
-$("#sB3").on("click", function() {
-    console.log("save");
-});
-$("#sB4").on("click", function() {
-    console.log("save");
-});
-$("#sB5").on("click", function() {
-    console.log("save");
-});
+// $("#sB11").on("click", function() {
+//     console.log("save");
+// });
+// $("#sB12").on("click", function() {
+//     console.log("save");
+// });
+// $("#sB13").on("click", function() {
+//     console.log("save");
+// });
+// $("#sB14").on("click", function() {
+//     console.log("save");
+// });
+// $("#sB15").on("click", function() {
+//     console.log("save");
+// });
+// $("#sB16").on("click", function() {
+//     console.log("save");
+// });
+// $("#sB17").on("click", function() {
+//     console.log("save");
+// });
+
+
+$("#9am").children("input").val(localStorage.getItem("9am"));
+
 
 
 
