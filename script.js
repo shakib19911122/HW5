594928895d0d3e313a7c3056fd9bb6870a4cb169
shakib <shakib1122@gmail.com>
2020-06-13 00:36:14 +1000

    function worked

--- script.js
+++ script.js
@@ -1,88 +1,93 @@
 
+// $(".saveBtn").on("click", function () {
+//   var timeOfday = $(this).parent().attr("id");
+//   var textContent = $("input").val().trim();
 
-//--onclick event to save user input to local storage---//
-$("#sB9").on("click", function() {
-    var timeOfday = $(this).parent().attr("id");
-    var textContent = $("input").val().trim();
-
-    localStorage.setItem(timeOfday, textContent);
-    console.log(timeOfday, textContent);
-});
-
-$("#sB10").on("click", function() {
+//   localStorage.setItem(timeOfday, textContent);
+//   console.log(timeOfday, textContent);
+// });
+$("#sB9").on("click", function () {
   var timeOfday = $(this).parent().attr("id");
-  var textContent = $("input").val().trim();
+  var textContent = $("#9").val().trim();
 
   localStorage.setItem(timeOfday, textContent);
   console.log(timeOfday, textContent);
 });
 
-$("#sB11").on("click", function() {
-    var timeOfday = $(this).parent().attr("id");
-    var textContent = $("input").val().trim();
+$("#sB10").on("click", function () {
+  var timeOfday = $(this).parent().attr("id");
+  var textContent = $("#10").val().trim();
 
-    localStorage.setItem(timeOfday, textContent);
-    console.log(timeOfday, textContent);
+  localStorage.setItem(timeOfday, textContent);
+  console.log(timeOfday, textContent);
 });
 
-$("#sB12").on("click", function() {
+$("#sB11").on("click", function () {
   var timeOfday = $(this).parent().attr("id");
-    var textContent = $("input").val().trim();
+  var textContent = $("#11").val().trim();
 
-    localStorage.setItem(timeOfday, textContent);
-    console.log(timeOfday, textContent);
+  localStorage.setItem(timeOfday, textContent);
+  console.log(timeOfday, textContent);
 });
 
-$("#sB13").on("click", function() {
+$("#sB12").on("click", function () {
   var timeOfday = $(this).parent().attr("id");
-    var textContent = $("input").val().trim();
+  var textContent = $("#12").val().trim();
 
-    localStorage.setItem(timeOfday, textContent);
-    console.log(timeOfday, textContent);
+  localStorage.setItem(timeOfday, textContent);
+  console.log(timeOfday, textContent);
 });
 
-$("#sB14").on("click", function() {
+$("#sB13").on("click", function () {
   var timeOfday = $(this).parent().attr("id");
-    var textContent = $("input").val().trim();
+  var textContent = $("#13").val().trim();
 
-    localStorage.setItem(timeOfday, textContent);
-    console.log(timeOfday, textContent);
+  localStorage.setItem(timeOfday, textContent);
+  console.log(timeOfday, textContent);
 });
 
-$("#sB15").on("click", function() {
+$("#sB14").on("click", function () {
   var timeOfday = $(this).parent().attr("id");
-    var textContent = $("input").val().trim();
+  var textContent = $("#14").val().trim();
 
-    localStorage.setItem(timeOfday, textContent);
-    console.log(timeOfday, textContent);
+  localStorage.setItem(timeOfday, textContent);
+  console.log(timeOfday, textContent);
 });
 
-$("#sB16").on("click", function() {
+$("#sB15").on("click", function () {
   var timeOfday = $(this).parent().attr("id");
-    var textContent = $("input").val().trim();
+  var textContent = $("#15").val().trim();
 
-    localStorage.setItem(timeOfday, textContent);
-    console.log(timeOfday, textContent);
+  localStorage.setItem(timeOfday, textContent);
+  console.log(timeOfday, textContent);
 });
 
-$("#sB17").on("click", function() {
+$("#sB16").on("click", function () {
   var timeOfday = $(this).parent().attr("id");
-    var textContent = $("input").val().trim();
+  var textContent = $("#16").val().trim();
 
-    localStorage.setItem(timeOfday, textContent);
-    console.log(timeOfday, textContent);
+  localStorage.setItem(timeOfday, textContent);
+  console.log(timeOfday, textContent);
+});
+
+$("#sB17").on("click", function () {
+  var timeOfday = $(this).parent().attr("id");
+  var textContent = $("#17").val().trim();
+
+  localStorage.setItem(timeOfday, textContent);
+  console.log(timeOfday, textContent);
 });
 
 
 $("#9am").children("input").val(localStorage.getItem("9am"));
-$("#10am").children("input").val(localStorage.getItem("9am"));
-$("#11am").children("input").val(localStorage.getItem("9am"));
-$("12pm").children("input").val(localStorage.getItem("9am"));
-$("#1pm").children("input").val(localStorage.getItem("9am"));
-$("#2pm").children("input").val(localStorage.getItem("9am"));
-$("#3pm").children("input").val(localStorage.getItem("9am"));
-$("#3pm").children("input").val(localStorage.getItem("9am"));
-$("#5pm").children("input").val(localStorage.getItem("9am"));
+$("#10am").children("input").val(localStorage.getItem("10am"));
+$("#11am").children("input").val(localStorage.getItem("11am"));
+$("12pm").children("input").val(localStorage.getItem("12pm"));
+$("#1pm").children("input").val(localStorage.getItem("13pm"));
+$("#2pm").children("input").val(localStorage.getItem("14pm"));
+$("#3pm").children("input").val(localStorage.getItem("15pm"));
+$("#3pm").children("input").val(localStorage.getItem("16pm"));
+$("#5pm").children("input").val(localStorage.getItem("17pm"));
 
 
 
