443587d000843bf6f6121fd9f75a963ed877ac2b
shakib <shakib1122@gmail.com>
2020-06-13 15:05:24 +1000

    cleaner version

--- script.js
+++ script.js
@@ -3,82 +3,91 @@ var date = new Date();
 $("#currentDay").append(date);
 
 
-
-
-
-
-
-$("#sB9").on("click", function () {
+$(".btn").on("click",function(){
   var timeOfday = $(this).parent().attr("id");
-  var textContent = $("#9").val().trim();
+  var textContent = $(this).siblings("input").val();
+  console.log(textContent);
 
   localStorage.setItem(timeOfday, textContent);
   console.log(timeOfday, textContent);
-});
 
-$("#sB10").on("click", function () {
-  var timeOfday = $(this).parent().attr("id");
-  var textContent = $("#10").val().trim();
+})
 
-  localStorage.setItem(timeOfday, textContent);
-  console.log(timeOfday, textContent);
-});
 
-$("#sB11").on("click", function () {
-  var timeOfday = $(this).parent().attr("id");
-  var textContent = $("#11").val().trim();
 
-  localStorage.setItem(timeOfday, textContent);
-  console.log(timeOfday, textContent);
-});
 
-$("#sB12").on("click", function () {
-  var timeOfday = $(this).parent().attr("id");
-  var textContent = $("#12").val().trim();
+//pleae ignor below code 
+// $("#sB9").on("click", function () {
+//   var timeOfday = $(this).parent().attr("id");
+//   var textContent = $("#9").val().trim();
 
-  localStorage.setItem(timeOfday, textContent);
-  console.log(timeOfday, textContent);
-});
+//   localStorage.setItem(timeOfday, textContent);
+//   console.log(timeOfday, textContent);
+// });
 
-$("#sB13").on("click", function () {
-  var timeOfday = $(this).parent().attr("id");
-  var textContent = $("#13").val().trim();
+// $("#sB10").on("click", function () {
+//   var timeOfday = $(this).parent().attr("id");
+//   var textContent = $("#10").val().trim();
 
-  localStorage.setItem(timeOfday, textContent);
-  console.log(timeOfday, textContent);
-});
+//   localStorage.setItem(timeOfday, textContent);
+//   console.log(timeOfday, textContent);
+// });
 
-$("#sB14").on("click", function () {
-  var timeOfday = $(this).parent().attr("id");
-  var textContent = $("#14").val().trim();
+// $("#sB11").on("click", function () {
+//   var timeOfday = $(this).parent().attr("id");
+//   var textContent = $("#11").val().trim();
 
-  localStorage.setItem(timeOfday, textContent);
-  console.log(timeOfday, textContent);
-});
+//   localStorage.setItem(timeOfday, textContent);
+//   console.log(timeOfday, textContent);
+// });
 
-$("#sB15").on("click", function () {
-  var timeOfday = $(this).parent().attr("id");
-  var textContent = $("#15").val().trim();
+// $("#sB12").on("click", function () {
+//   var timeOfday = $(this).parent().attr("id");
+//   var textContent = $("#12").val().trim();
 
-  localStorage.setItem(timeOfday, textContent);
-  console.log(timeOfday, textContent);
-});
+//   localStorage.setItem(timeOfday, textContent);
+//   console.log(timeOfday, textContent);
+// });
 
-$("#sB16").on("click", function () {
-  var timeOfday = $(this).parent().attr("id");
-  var textContent = $("#16").val().trim();
+// $("#sB13").on("click", function () {
+//   var timeOfday = $(this).parent().attr("id");
+//   var textContent = $("#13").val().trim();
 
-  localStorage.setItem(timeOfday, textContent);
-  console.log(timeOfday, textContent);
-});
+//   localStorage.setItem(timeOfday, textContent);
+//   console.log(timeOfday, textContent);
+// });
 
-$("#sB17").on("click", function () {
-  var timeOfday = $(this).parent().attr("id");
-  var textContent = $("#17").val().trim();
+// $("#sB14").on("click", function () {
+//   var timeOfday = $(this).parent().attr("id");
+//   var textContent = $("#14").val().trim();
 
-  localStorage.setItem(timeOfday, textContent);
-  console.log(timeOfday, textContent);
-});
+//   localStorage.setItem(timeOfday, textContent);
+//   console.log(timeOfday, textContent);
+// });
+
+// $("#sB15").on("click", function () {
+//   var timeOfday = $(this).parent().attr("id");
+//   var textContent = $("#15").val().trim();
+
+//   localStorage.setItem(timeOfday, textContent);
+//   console.log(timeOfday, textContent);
+// });
+
+// $("#sB16").on("click", function () {
+//   var timeOfday = $(this).parent().attr("id");
+//   var textContent = $("#16").val().trim();
+
+//   localStorage.setItem(timeOfday, textContent);
+//   console.log(timeOfday, textContent);
+// });
+
+// $("#sB17").on("click", function () {
+//   var timeOfday = $(this).parent().attr("id");
+//   var textContent = $("#17").val().trim();
+
+//   localStorage.setItem(timeOfday, textContent);
+//   console.log(timeOfday, textContent);
+// });
 
 
 $("#9am").children("input").val(localStorage.getItem("9am"));
